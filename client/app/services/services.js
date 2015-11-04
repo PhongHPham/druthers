285046c2af8bd8c7f3ca06f71468b73ea2e20241
Phong Pham <phizzy@gmail.com>
2015-11-03 21:29:50 -0800

    Return getMatchWithTwitterHandle function and isolate imageUrl

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -3,11 +3,14 @@ app.factory('twitterServices', function ($http) {
   var getMatchWithTwitterHandle = function (twitterHandle) {
     return $http({
       method: 'GET',
-      url: '/api/twitter/:twitterHandle'
+      url: '/api/twitter/' + twitterHandle
     })
     .then(function (response) {
+      console.log('returning the pic!!!! :', response.data.imageUrl)
       return response.data;
     });
   };
-
+  return {
+    getMatchWithTwitterHandle: getMatchWithTwitterHandle
+  };
 });
