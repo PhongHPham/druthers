6b8076cc193cbce29e89d63feba12c48814b0b8f
Phong Pham <phizzy@gmail.com>
2015-11-05 05:24:12 -0800

    Tweak match logic to account not for local trait difference but difference of total trait sum scores

--- server/generateMatch.js
+++ server/generateMatch.js
@@ -6,11 +6,18 @@
     var candidateMatch; 
     // make difs object for debugging
     var difs = {};
+    var userPersonalityScore = 0;
+    var candidateScore = 0;
+
+
+    // for (var userTrait in userPersonality) {
+    //   userPersonalityScore += userPersonality[userTrait];
+    // }
 
     // console.log(userPersonality)
     // console.log(candidates[0])
   // iterate over candidates array
-    for (var i = 0; i <= 0; i++) {
+    for (var i = 0; i < candidates.length; i++) {
       // current difference starts as 0
       var curDif = 0;
       // for each candidate, loop over their user's personality traits object
@@ -18,9 +25,12 @@
         // console.log(trait)
         // for each trait calculate distance of absolute trait values
         // between userPersonality and current candidate's traits
-        console.log(trait)
-        console.log(candidates[i][trait])
-        curDif += Math.abs(userPersonality[trait] - candidates[i][trait]);
+        // console.log(trait)
+        // console.log(candidates[i][trait])
+        userPersonalityScore += userPersonality[trait];
+        candidateScore += candidates[i][trait];
+
+        curDif = Math.abs(userPersonalityScore - candidateScore);
         // console.log(curDif)
         // check if difference lower than lowest difference
         if (curDif < lowestDif) {
@@ -32,6 +42,9 @@
           // candidateMatch is going be current candidate if it's difference is still lowest
           candidateMatch = candidates[i];
         }
+        // reset user and candidate personality scores
+        userPersonalityScore = 0;
+        candidateScore = 0;
       }
     } 
     // check output of what's in our difs object (expecting all the candidates)
