070a6bccefe1444f8695ce0dc1f7519c02455a68
Phong Pham <phizzy@gmail.com>
2015-11-05 06:12:49 -0800

    Continue to debug match logic

--- server/generateMatch.js
+++ server/generateMatch.js
@@ -30,22 +30,24 @@
         userPersonalityScore += userPersonality[trait];
         candidateScore += candidates[i][trait];
 
-        curDif = Math.abs(userPersonalityScore - candidateScore);
-        // console.log(curDif)
-        // check if difference lower than lowest difference
-        if (curDif < lowestDif) {
-          // set key value pair of current candidate and current difference to 
-          // check what we're getting
-          difs[candidates[i].name] = curDif;
-          // set current difference as lowest difference
-          lowestDif = curDif;
-          // candidateMatch is going be current candidate if it's difference is still lowest
-          candidateMatch = candidates[i];
-        }
-        // reset user and candidate personality scores
-        userPersonalityScore = 0;
-        candidateScore = 0;
+
+      curDif = Math.abs(userPersonalityScore - candidateScore);
       }
+
+      // console.log(curDif)
+      // check if difference lower than lowest difference
+      if (curDif < lowestDif) {
+        // set key value pair of current candidate and current difference to 
+        // check what we're getting
+        difs[candidates[i].name] = curDif;
+        // set current difference as lowest difference
+        lowestDif = curDif;
+        // candidateMatch is going be current candidate if it's difference is still lowest
+        candidateMatch = candidates[i];
+      }
+      // reset user and candidate personality scores
+      userPersonalityScore = 0;
+      candidateScore = 0;
     } 
     // check output of what's in our difs object (expecting all the candidates)
     // console.log(difs);
