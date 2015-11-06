234d8f1b533a99e4f1a811db91c5afc134f7a4ea
Phong Pham <phizzy@gmail.com>
2015-11-05 21:18:01 -0800

    Add this.candidateName to also display name of candidate match

--- client/app/controllers/twitterInputController.js
+++ client/app/controllers/twitterInputController.js
@@ -3,15 +3,14 @@
 
 app.controller('TwitterInputController', ['twitterServices', function (twitterServices) {
   this.twitterHandle = null;
+  this.candidateName = 'Enter your twitter handle!';
   // when user submits twitter handle getCandidateMatchpic calls factory function
   // var self = this;
   this.getCandidateMatch = function () {
     twitterServices.getMatchWithTwitterHandle(this.twitterHandle)
       .then(function (candidate) {
-        // this.pic = pic
-        // console.log(this.pic)
-        console.log('AHA heres the picUrl:', candidate);
         this.candidate = candidate;
+        this.candidateName = 'You\'ve matched with ' + this.candidate.name;
         return this.candidate;
     }.bind(this));
   };
