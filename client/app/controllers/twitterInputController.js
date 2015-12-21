7afd7feceb71b2241a050fde81b292e7b267c082
Phong Pham <phizzy@gmail.com>
2015-12-20 16:57:39 -0800

    Tidy up code

--- client/app/controllers/twitterInputController.js
+++ client/app/controllers/twitterInputController.js
@@ -1,11 +1,8 @@
-// angular.module('druthers.services', [])
-
 
 app.controller('TwitterInputController', ['twitterServices', function (twitterServices) {
   this.twitterHandle = null;
   this.candidateName = '';
   // when user submits twitter handle getCandidateMatchpic calls factory function
-  // var self = this;
   this.getCandidateMatch = function() {
     twitterServices.getMatchWithTwitterHandle(this.twitterHandle)
       .then(function (responseData) {
