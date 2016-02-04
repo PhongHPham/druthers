0126eb56a003d031ff12bd29e06bff371391e289
Phong Pham <phizzy@gmail.com>
2016-02-04 09:50:01 -0500

    Update candidates array removing dropouts

--- server/createCandidates.js
+++ server/createCandidates.js
@@ -7,17 +7,12 @@ var mongoose = require('mongoose');
 var candidates = [['Donald Trump', 'realDonaldTrump', 'https://i.imgur.com/Agz1KPg.jpg'],
                   ['Ben Carson', 'RealBenCarson', 'https://i.imgur.com/tdyvY5C.jpg'],
                   ['Bernie Sanders', 'BernieSanders', 'https://i.imgur.com/egXkZtv.jpg'],
-                  ['Bobby Jindal', 'BobbyJindal', 'https://i.imgur.com/g3flAoU.jpg'],
                   ['Carly Fiorina', 'CarlyFiorina', 'https://i.imgur.com/aMU1VDr.jpg'],
                   ['Chris Christie', 'ChrisChristie', 'https://i.imgur.com/bWR0P8H.jpg'],
                   ['Hillary Clinton', 'HillaryClinton', 'https://i.imgur.com/qu6BBGT.jpg'],
                   ['Jeb Bush', 'JebBush', 'https://i.imgur.com/NTmJ5Ct.jpg'],
                   ['John Kasich', 'JohnKasich', 'https://i.imgur.com/o7JyBOC.jpg'],
-                  ['Lawrence Lessig', 'lessig', 'https://i.imgur.com/xlU7YZS.jpg'],
                   ['Marco Rubio', 'marcorubio', 'https://i.imgur.com/tkuphbJ.jpg'],
-                  ['Martin O\'Malley', 'MartinOMalley', 'https://i.imgur.com/oL8ijqx.jpg'],
-                  ['Mike Huckabee', 'GovMikeHuckabee', 'https://i.imgur.com/5uZIgnV.jpg'],
-                  ['Rand Paul', 'RandPaul', 'https://i.imgur.com/2w0Vm8l.jpg'],
                   ['Ted Cruz', 'tedcruz', 'https://i.imgur.com/JKs2Dhk.jpg']
 ];
 
